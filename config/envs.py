759e862e09780dbb0471f889889d739e4a272d5d
Vladimir <58716739+Vladimir-Cool@users.noreply.github.com>
2024-10-01 11:55:32 +0800

    add pydantic settings.

---
+++ config/envs.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+
+load_dotenv(dotenv_path="config/.env")
+
+
+class SettingDjango(BaseSettings):
+    """Класс настроек для ДБ"""
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
+
+
+settings_django = SettingDjango()
--- djangoexample/djangoexample/settings.py
+++ djangoexample/djangoexample/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from config.envs import settings_django
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+_t-54$h%i75sh@y@95592ab9ht^ow8wa+z06)^fv$g_sx55gi'
+SECRET_KEY = settings_django.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
